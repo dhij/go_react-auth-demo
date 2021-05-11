1a27900edea10ac3ddfc603d49ecf5178f965102
David Hwang <davidhwang.ij@gmail.com>
2021-05-10 20:57:00 -0400

    react bootstrap navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
-  return <h1>This is a header</h1>
+  return (
+    <>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>Go React Auth</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/signup'>Sign Up</Nav.Link>
+              <Nav.Link href='/login'>Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  )
 }
 
 export default Header
