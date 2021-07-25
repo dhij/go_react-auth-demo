73e9f55e8799af94151a8fd34d10faa2d5993316
David Hwang <davidhwang.ij@gmail.com>
2021-07-25 00:48:36 -0400

    Added redux login_success part

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,28 +11,12 @@ import SignupScreen from './screens/SignupScreen'
 const App = () => {
   const [firstName, setFirstName] = useState('')
 
-  useEffect(() => {
-    ;(async () => {
-      const response = await fetch('http://localhost:8081/api/user', {
-        headers: { 'Content-Type': 'application/json' },
-        credentials: 'include',
-      })
-
-      const data = await response.json()
-      setFirstName(data.first_name)
-    })()
-  })
-
   return (
     <Router>
       <Header firstName={firstName} setFirstName={setFirstName} />
       <main>
         <Container>
-          <Route
-            path='/'
-            exact
-            component={() => <HomeScreen firstName={firstName} />}
-          />
+          <Route path='/' exact component={HomeScreen} />
           <Route path='/signup' component={SignupScreen} />
           <Route path='/login' component={LoginScreen} />
         </Container>
